069e4a9221abc05a609d4124a15dd7b1c33ccc37
humphrey <humphrey0572@outlook.com>
2023-06-10 22:21:32 +0800

    add main folder

---
+++ main/inc/main.h
@@ -0,0 +1,29 @@
+/******************************************************************************
+ Copyright (c) 2023 Versuni Co.,Ltd. 
+ All rights reserved.
+
+ [Module Name]:
+        main.h
+ [Date]:
+        2023-06-10
+ [Author]:
+        Carson Qian 
+ [Reversion History]:
+        v1.0: draft
+*******************************************************************************/
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+
+int main(void);
+
+#ifdef __cplusplus 
+}
+#endif
+
+
+#endif /* _MAIN_H_ */
---
+++ main/src/main.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+ Copyright (c) 2023 Versuni Co.,Ltd. 
+ All rights reserved.
+
+ [Module Name]:
+        main.c
+ [Date]:
+        2023-06-10
+ [Author]:
+        Carson Qian 
+ [Reversion History]:
+        v1.0: draft
+*******************************************************************************/
+#include "main.h"
+
+int main(void)
+{
+    int a;
+    int b;
+    int sum;
+
+    sum = a + b;
+
+    return 0;
+}
